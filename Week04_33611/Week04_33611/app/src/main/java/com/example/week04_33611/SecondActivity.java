72e24dc1b0ada21c6293db9d01aaa8b34b5c80cf
islahatul93 <islahatul93@gmail.com>
2021-03-12 20:12:52 +0700

    Four

---
+++ Week04_33611/Week04_33611/app/src/main/java/com/example/week04_33611/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.example.week04_33611;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        setTitle("Week04_33611");
+    }
+}
